349acd4ee1728cd6592b4be78b2595282f0a8564
murillobazz <murillobazilio@gmail.com>
2023-05-30 08:04:57 -0300

    changes to render deployment

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -84,8 +86,7 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${quantity} people</p><p>${date}</p>`);
 })
 
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
