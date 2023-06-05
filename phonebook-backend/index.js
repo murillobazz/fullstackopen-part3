c04187ea9a4b5260c95c3f1f2fa1e03507221b38
murillobazz <murillobazilio@gmail.com>
2023-06-05 09:20:46 -0300

    exercises 3.15 - 3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -5,25 +5,37 @@ const cors = require('cors');
 const app = express();
 const Person = require('./models/person');
 
-app.use(express.json()); // Express middleware to parse json responses
-app.use(express.static('build'));
-app.use(cors());
+app.use(express.json()); // Express middleware to parse json responses, must be between the first ones always.
+app.use(express.static('build')); // Express middleware to serve static files through the backend 
+// (parameter is the the build directory), it makes it that the backend can be accessed through the browser.
 
+app.use(cors()); // Express middleware to handle cors error on the server side;
 // Morgan is used to intercept every API call and log its details to the console
 morgan.token('body', (req, res) => JSON.stringify(req.body)); // Here we define a custom token to be shown in the message that'll be logged
 // Below, we call express.use on morgan with the 'fields' that'll be shown in the console message (per docs)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-// The GET call has been modified to execute a callback function with a MongoDB query (find({})), it works perfectly when connected to a Mongo DB.
+const ErrorHandler = (err, req, res, next) => {
+  consoleError(err.message);
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(err);
+}
+app.use(ErrorHandler); // Middleware for handling errors, should be stated last.
+
+// The GET ALL call has been modified to execute a callback function with a MongoDB query (find({})),
+// it works perfectly when connected to a Mongo DB.
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(notes => {
-    res.json(notes);
+  Person.find({}).then(persons => {
+    res.json(persons);
   })
 })
 
+// POST Person - Already working with the database, using the Person model.
 app.post('/api/persons', (req, res) => {
   const body = req.body;
-  // const alreadyExists = Person.findOne({ name : body.name });
 
   if (!body.name || !body.number) {
     return res.status(400).json({
@@ -31,40 +43,70 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  // if (alreadyExists) {
-  //   return res.status(400).json({
-  //     error: "name must be unique"
-  //   })
-  // }
-
   const newPerson = new Person({
     name: body.name,
     number: body.number,
   });
 
-  newPerson.save().then(savedPerson => {
-    res.json(savedPerson);    
-  })
-
+  newPerson.save()
+    .then(savedPerson => {
+      res.json(savedPerson);    
+    })
+    .catch(err => next(err));
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person);
-  })
+// GET By ID
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
+// PUT By Name
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
 
-  res.status(204).end();
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson);
+    })
+    .catch(err => next(err))
 })
 
+// DELETE Person
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      if(!result) {
+        console.log(`This person had already been removed.`)
+      }
+      res.status(204).end();
+    })
+    .catch(err => next(err));
+})
+
+// GET /info
 app.get('/info', (req, res) => {
-  const quantity = persons.length;
   const date = new Date();
-  res.send(`<p>Phonebook has info for ${quantity} people</p><p>${date}</p>`);
+
+  Person.find({})
+    .then(persons => {
+      const quantity = persons.length;
+      res.send(`<p>Phonebook has info for ${quantity} people</p><p>${date}</p>`);
+    })
+    .catch(err => next(err));
+
 })
 
 const PORT = process.env.PORT;
